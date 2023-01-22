ed51410484b1901d65e4bd66f2e13b1e09bc5a1d
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-22 16:34:16 +0530

    Edit Todo feature complete

---
+++ app/src/main/java/com/example/localstorage/features/todo/EditTodoViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.localstorage.features.todo
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.localstorage.data.db.DBAccessPoint
+import com.example.localstorage.data.model.Todo
+import kotlinx.coroutines.launch
+
+class EditTodoViewModel(application: Application) : AndroidViewModel(application) {
+
+    fun updateTodo(todo: Todo) {
+        viewModelScope.launch {
+            DBAccessPoint.getDB(getApplication()).todoDao().updateTodo(todo)
+        }
+    }
+
+}
