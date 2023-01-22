bf50ec548ddaca9d97346580c03c7c7781e1440e
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-22 13:06:26 +0530

    Updated TodoActivity.kt and TodoViewModel.kt to get and add todos

--- app/src/main/java/com/example/localstorage/feat/todo/TodoActivity.kt
+++ app/src/main/java/com/example/localstorage/feat/todo/TodoActivity.kt
@@ -2,15 +2,64 @@ package com.example.localstorage.feat.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import com.example.localstorage.R
+import com.example.localstorage.data.model.Employee
+import com.example.localstorage.data.model.Todo
+import com.example.localstorage.databinding.ActivityMainBinding
+import com.example.localstorage.databinding.ActivityTodoBinding
+import com.example.localstorage.util.Resource
 
 class TodoActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityTodoBinding
     private val viewModel by viewModels<TodoViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_todo)
+        binding = ActivityTodoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var todoNumber = 100
+        getAllTodos()
+
+        binding.apply {
+            btnAddTodo.setOnClickListener {
+                viewModel.addTodo("Todo Title $todoNumber", "Todo Description $todoNumber")
+                todoNumber++
+            }
+
+            btnShowAllTodo.setOnClickListener {
+                getAllTodos()
+            }
+        }
+
+    }
+
+    private fun getAllTodos() {
+        viewModel.getAllTodos().observe(this) {
+            when (it) {
+                is Resource.Default -> {
+                    binding.cpiLoading.visibility = View.GONE
+                }
+                is Resource.Failure -> {
+                    binding.cpiLoading.visibility = View.GONE
+                }
+                is Resource.Loading -> {
+                    binding.cpiLoading.visibility = View.VISIBLE
+                }
+                is Resource.Success -> {
+                    setText(it.value)
+                }
+            }
+        }
+    }
+
+    private fun setText(value: List<Todo>) {
+        var data = ""
+        value.forEach {
+            data += "${it.id}\n" + it.title + "\n" + it.description + "\n\n\n"
+        }
+        binding.tvEmployees.text = data
     }
 }
--- app/src/main/java/com/example/localstorage/feat/todo/TodoViewModel.kt
+++ app/src/main/java/com/example/localstorage/feat/todo/TodoViewModel.kt
@@ -2,12 +2,39 @@ package com.example.localstorage.feat.todo
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import com.example.localstorage.data.db.DBAccessPoint
+import com.example.localstorage.data.model.Todo
+import com.example.localstorage.util.FailureStatus
+import com.example.localstorage.util.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class TodoViewModel(
     application: Application
 ) : AndroidViewModel(application) {
 
+    fun getAllTodos() = liveData(Dispatchers.IO) {
+        emit(Resource.Loading)
+        try {
+            emit(Resource.Success(DBAccessPoint.getDB(getApplication()).todoDao().getAllTodos()))
+        } catch (exception: Exception) {
+            emit(Resource.Failure(FailureStatus.API_FAIL, message = exception.message ?: "Error!"))
+        }
+    }
 
+    fun addTodo(todoTitle: String, todoDescription: String) {
+        viewModelScope.launch {
+            DBAccessPoint.getDB(getApplication()).todoDao()
+                .addTodo(
+                    Todo(
+                        title = todoTitle,
+                        description = todoDescription
+                    )
+                )
+        }
+    }
 
 
 }
