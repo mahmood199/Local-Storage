8080d0954c0c14b52d5dcff3220219233f4c273c
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-22 12:06:18 +0530

    Package restructuring

--- app/src/main/java/com/example/localstorage/feat/EmployeeViewModel.kt
+++ app/src/main/java/com/example/localstorage/feat/EmployeeViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.localstorage.ui
+package com.example.localstorage.feat
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
--- app/src/main/java/com/example/localstorage/feat/MainActivity.kt
+++ app/src/main/java/com/example/localstorage/feat/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.localstorage.ui
+package com.example.localstorage.feat
 
 import android.os.Bundle
 import android.text.TextUtils
