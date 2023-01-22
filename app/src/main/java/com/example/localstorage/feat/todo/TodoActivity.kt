5c1e483898a0f527e5e291ac6a811505b29eeeb0
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-22 12:14:25 +0530

    UI added

---
+++ app/src/main/java/com/example/localstorage/feat/todo/TodoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.localstorage.feat.todo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.localstorage.R
+
+class TodoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo)
+    }
+}
