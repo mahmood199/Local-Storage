cef21d940a2b028db0b8f40fb3fb853bd963a0c0
MSI KATANA <gaudentas199@gmail.com>
2023-05-02 09:06:43 +0530

    Added new chat activity for chats

---
+++ app/src/main/java/com/example/localstorage/features/chat/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.example.localstorage.features.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.localstorage.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
